536ea698550c4c52c961daf469f5d55ff5011b6e
Seda Tarverdyan <66208259+sedatarverdyan@users.noreply.github.com>
2024-09-08 02:14:15 +0400

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -1,40 +1,48 @@
-
+from openai import OpenAI
 import streamlit as st
 
-# Initialize session state for chat messages
-if 'chat_messages' not in st.session_state:
-    st.session_state.chat_messages = []
+api_key = 'your-secret-key'
+
+client = OpenAI(api_key=api_key)
+
 
 st.title("Image Generator")
 
 # Function to update chat messages
 def update_chat_messages(message):
-    st.session_state.chat_messages.append(message)
+    st.session_state.chat_messages = []
+    
+ 
+st.header("Let's generate a picture together!")
 
-with st.chat_message("assistant"):
-    st.write("Let's generate a picture together!")
-    prompt = st.chat_input("Describe a picture")
-    if prompt:
-        update_chat_messages(("user", prompt))
+prompt1=st.text_input("Describe a picture and press Enter to apply")
 
-# Display chat messages
-for role, message in st.session_state.chat_messages:
-    with st.chat_message(role):
-        st.write(message)
+# Check if prompt1 is None
+if prompt1:
+    update_chat_messages(("user", prompt1))
+    with st.chat_message("user"):
+            st.write(prompt1)
+
 
 option1 = st.selectbox(
     label="Select style",
-    options=("Van Gogh Style","Cartoon Style","Sketch Style","Watercolor Style",
+    options=("None","Van Gogh Style","Cartoon Style","Sketch Style","Watercolor Style",
              "Impressionist Style","Pop Art Style","Realistic Oil Painting Style",
              "Minimalist Style","Surrealism Style","Pointillism Style","Fauvism Style")
 )
 
 option2 = st.selectbox(
     label="Select size",
-    options=("256x256 pixels","128x128 pixels","64x64 pixels","Custom Size")
+    options=("1024x1024","1024x1792", "1792x1024")
 )
 
-if st.button("Generate"):
-    # Perform image generation based on user input and selected options
-    # Add your image generation code here
-    st.write("Image generated!")
+if st.button("Generate") and prompt1 is not None:
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=f"{prompt1} Generate image with this prompt, that has a {option1}",
+        size=option2,
+        quality="standard",
+        n=1,
+    )
+    image_url = response.data[0].url
+    st.image(image_url, caption='Generated image')
